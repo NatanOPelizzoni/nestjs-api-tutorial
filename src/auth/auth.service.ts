47fbefbacfe715440f9b803702451a3106c3f077
Natan O Pelizzoni <natan2624@gmail.com>
2023-03-29 00:57:36 -0300

    Updated models and updated auth service to save user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,37 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { Prisma } from '@prisma/client'
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
+    constructor(private prisma: PrismaService) { }
 
-    constructor(private prisma: PrismaService) {
-
-    }
-    signup(dto: AuthDto) {
-        return { msg: 'I have signed up' };
+    async signup(dto: AuthDto) {
+        //Generate the password hash
+        const hash = await argon.hash(dto.password);
+        //save the new user in the db
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash: hash,
+                }
+            });
+            delete user.hash;
+            //return the saved user
+            return user;
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException(
+                        'Email already exists',
+                    );
+                }
+            }
+            throw error;
+        }
     }
 
     signin() {
